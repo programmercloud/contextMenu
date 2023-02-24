43b8525e8605eda92e47473737bdfb8ec6b21c6b
programmercloud <88435015+programmercloud@users.noreply.github.com>
2023-02-24 12:19:00 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+feather.replace();
+// This line of JavaScript code is used to replace all the feather icons in the
+//HTML document with their SVG representations, which are styled and animated with CSS
